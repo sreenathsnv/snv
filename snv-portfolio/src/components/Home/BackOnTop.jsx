7e29a1ade648bad51c44bddd6daa3e616a556738
SNV <sreenathsnvaltpb@gmail.com>
2024-05-27 09:58:35 +0530

    minor changes : class to className

--- snv-portfolio/src/components/Home/BackOnTop.jsx
+++ snv-portfolio/src/components/Home/BackOnTop.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 import '../../CSS/backontop.css'
-
 const BackOnTop = () => {
   return (
     <div className="wrapper-btn">
         <a href="/#nav">
-        <button class="Btn">
-  <svg height="1.2em" class="arrow" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path></svg>
-<p class="text">Back to Top</p>
-    </button>
+
+        <button className="Btn">
+          <svg height="1.2em" class="arrow" viewBox="0 0 512 512"><path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path></svg>
+          <p className="text">Back to Top</p>
+        </button>
         </a>
     </div>
   )
