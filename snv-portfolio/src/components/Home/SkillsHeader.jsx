de010b63566e2d24e546ca1b14f7bceb3109e6fc
SNV <sreenathsnvaltpb@gmail.com>
2024-05-27 10:34:22 +0530

    proj::core:008 : Animation for skills header added

--- snv-portfolio/src/components/Home/SkillsHeader.jsx
+++ snv-portfolio/src/components/Home/SkillsHeader.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, spring, useAnimation, useInView } from "framer-motion"
+
 
 const SkillsHeader = () => {
+
+  const contRef  = useRef()
+  const view = useInView(contRef,{once:true})
+  const containerAnimate = useAnimation()
   return (
-        <div class="skills-head">
-            <p class="skills-heading">Skills</p>
-            <p class="skills-quote">"The expert in anything was once a beginner"</p>
-        </div>
+        <motion.div 
+        ref={contRef}
+        initial={{opacity:0,
+          x:-20,
+        }}
+        whileInView={
+          {
+            opacity:1,
+            x:0,
+
+        }}
+        transition={{duration:1,delay:.05}}
+        className="skills-head">
+            <p className="skills-heading">Skills</p>
+            <p className="skills-quote">"The expert in anything was once a beginner"</p>
+        </motion.div>
   )
 }
 
